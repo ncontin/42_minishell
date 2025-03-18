f3dc95855c329daebb7f38e4244a84ab93b30713
Nicolo Contin <nicocontin@gmail.com>
2025-03-18 19:22:12 +0100

    export done

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -6,49 +6,103 @@
 /*   By: ncontin <ncontin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 16:52:00 by ncontin           #+#    #+#             */
-/*   Updated: 2025/03/18 18:08:19 by ncontin          ###   ########.fr       */
+/*   Updated: 2025/03/18 19:21:58 by ncontin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int	ft_strcmp(char *s1, char *s2)
+static int	find_equal(char *str)
 {
 	int	i;
 
 	i = 0;
-	while (s1[i] && s1[i] == s2[i])
+	while (str[i])
+	{
+		if (str[i] == '=')
+			return (i);
 		i++;
-	return (s1[i] - s2[i]);
+	}
+	return (-1);
+}
+
+static char	**copy_env(char **envp)
+{
+	int		i;
+	char	**sorted_env;
+
+	i = 0;
+	while (envp[i])
+		i++;
+	sorted_env = malloc(sizeof(char *) * (i + 1));
+	if (!sorted_env)
+		return (NULL);
+	i = 0;
+	while (envp[i])
+	{
+		sorted_env[i] = ft_strdup(envp[i]);
+		i++;
+	}
+	sorted_env[i] = NULL;
+	return (sorted_env);
+}
+
+static int	find_len(char *s1, char *s2)
+{
+	int	s1_len;
+	int	s2_len;
+	int	len;
+
+	s2_len = ft_strlen(s2);
+	s1_len = ft_strlen(s1);
+	if (s1_len < s2_len)
+		len = s1_len;
+	else
+		len = s2_len;
+	return (len);
+}
+
+void	sort_env(char **sorted_env)
+{
+	int		i;
+	int		j;
+	char	*substr1;
+	char	*substr2;
+	char	*temp;
+
+	i = 0;
+	while (sorted_env[i])
+	{
+		j = i;
+		while (sorted_env[++j])
+		{
+			substr1 = ft_substr(sorted_env[i], 0, find_equal(sorted_env[i]));
+			substr2 = ft_substr(sorted_env[j], 0, find_equal(sorted_env[j]));
+			if (ft_strncmp(substr1, substr2, find_len(substr1, substr2)) > 0)
+			{
+				temp = sorted_env[i];
+				sorted_env[i] = sorted_env[j];
+				sorted_env[j] = temp;
+			}
+			free(substr1);
+			free(substr2);
+		}
+		i++;
+	}
 }
 
 void	ft_export(char **envp)
 {
-	int		i;
 	char	**sorted_env;
-	char	*min;
-	int		j;
+	int		i;
 
-	// sorted = malloc(sizeof(envp));
-	sorted_env = envp;
+	sorted_env = copy_env(envp);
+	sort_env(sorted_env);
 	i = 0;
-	while (sorted_env[i] && sorted_env[i + 1])
+	while (sorted_env[i])
 	{
 		printf("declare -x %s\n", sorted_env[i]);
-		printf("diff %d\n", ft_strcmp(envp[i + 1], envp[i]));
-		// printf("strchr: %s\n", ft_substr(envp[i], 0, )
-		j = i;
-		while (sorted_env[j] && sorted_env[j + 1])
-		{
-			if (ft_strcmp(envp[j + 1], envp[j]) < 0)
-			{
-				min = envp[j];
-				//		printf("min: %s\n", min);
-			}
-			j++;
-		}
 		i++;
 	}
-	printf("min: %s\n", min);
-	// export
+	free_array(sorted_env);
 }
