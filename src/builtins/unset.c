9e11d5f8094ebeb48d8896ac056c2d5c0d2cc555
Nicolo Contin <nicocontin@gmail.com>
2025-03-25 13:38:02 +0100

    refactor and fix unset function

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -6,13 +6,13 @@
 /*   By: ncontin <ncontin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 11:01:48 by ncontin           #+#    #+#             */
-/*   Updated: 2025/03/24 16:14:05 by ncontin          ###   ########.fr       */
+/*   Updated: 2025/03/25 13:37:04 by ncontin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	unset_env(t_env_node *current, t_env_node *prev,
+static void	del_env(t_env_node *current, t_env_node *prev,
 		t_env_node **envp_stack)
 {
 	if (prev == NULL)
@@ -26,51 +26,42 @@ static void	unset_env(t_env_node *current, t_env_node *prev,
 	free(current);
 }
 
-void	ft_unset(t_env *lst_env, char **args)
+static void	unset_env(t_env_node **env_stack, char *arg)
 {
 	t_env_node	*current;
 	t_env_node	*prev;
-	int			i;
+	t_env_node	*next;
+
+	current = *env_stack;
+	prev = NULL;
+	while (current)
+	{
+		if (ft_strncmp(arg, current->key, ft_strlen(current->key)) == 0)
+		{
+			next = current->next;
+			del_env(current, prev, env_stack);
+			current = next;
+		}
+		else
+		{
+			prev = current;
+			current = current->next;
+		}
+	}
+}
+
+void	ft_unset(t_env *lst_env, char **args)
+{
+	int	i;
 
 	if (!lst_env->envp_cp)
 		return ;
 	i = 1;
 	while (args[i])
 	{
-		current = *lst_env->envp_cp;
-		prev = NULL;
-		while (current)
-		{
-			if (ft_strncmp(args[i], current->key, ft_strlen(current->key)) == 0)
-			{
-				unset_env(current, prev, lst_env->envp_cp);
-				current = current->next;
-			}
-			else
-			{
-				prev = current;
-				current = current->next;
-			}
-		}
+		unset_env(lst_env->envp_cp, args[i]);
 		if (lst_env->envp_export && *lst_env->envp_export)
-		{
-			current = *lst_env->envp_export;
-			prev = NULL;
-			while (current)
-			{
-				if (ft_strncmp(args[i], current->key,
-						ft_strlen(current->key)) == 0)
-				{
-					unset_env(current, prev, lst_env->envp_export);
-					current = current->next;
-				}
-				else
-				{
-					prev = current;
-					current = current->next;
-				}
-			}
-		}
+			unset_env(lst_env->envp_export, args[i]);
 		i++;
 	}
 }
