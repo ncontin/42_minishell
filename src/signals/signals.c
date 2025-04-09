c4b31f1376556ac14ed19e4be93880a756890923
Nicolo Contin <nicocontin@gmail.com>
2025-04-09 12:35:51 +0200

    add readlines functions in signals

--- src/signals/signals.c
+++ src/signals/signals.c
@@ -6,7 +6,7 @@
 /*   By: ncontin <ncontin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 11:08:22 by ncontin           #+#    #+#             */
-/*   Updated: 2025/04/01 17:27:52 by ncontin          ###   ########.fr       */
+/*   Updated: 2025/04/09 12:35:23 by ncontin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,9 @@ void	sig_handler(int signal)
 {
 	if (signal == SIGINT)
 	{
-		printf("\nminishell> ");
+		printf("\n");
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
 	}
 }
