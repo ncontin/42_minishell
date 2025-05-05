8d5a6edf2abb6d9536ffd0f43e8183fa00b3261d
Anthonin Roulleau <aroullea@1D4.42angouleme.fr>
2025-05-05 10:24:00 +0200

    fix tilde

--- src/parsing/check_operator/is_tilde.c
+++ src/parsing/check_operator/is_tilde.c
@@ -6,7 +6,7 @@
 /*   By: aroullea <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 17:04:55 by aroullea          #+#    #+#             */
-/*   Updated: 2025/05/03 11:48:47 by aroullea         ###   ########.fr       */
+/*   Updated: 2025/05/05 10:21:17 by aroullea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,15 +63,18 @@ int	is_tilde(t_mini *mini)
 	current = mini->tokens;
 	while (current != NULL)
 	{
-		if (current->argument != NULL && current->argument[0] == '~')
+		if (current->quotes == NO_QUOTES)
 		{
-			if (current->argument[1] == '\0' || current->argument[1] == '/')
+			if (current->argument != NULL && current->argument[0] == '~')
 			{
-				if (current->linked == FALSE || (current->linked == TRUE
-						&& current->next->argument != NULL
-						&& current->next->argument[0] == '/'))
-					if (handle_tilde(&current->argument, mini) == 1)
-						return (1);
+				if (current->argument[1] == '\0' || current->argument[1] == '/')
+				{
+					if (current->linked == FALSE || (current->linked == TRUE
+								&& current->next->argument != NULL
+								&& current->next->argument[0] == '/'))
+						if (handle_tilde(&current->argument, mini) == 1)
+							return (1);
+				}
 			}
 		}
 		current = current->next;
